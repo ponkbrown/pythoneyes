881a5f94f54801d186201ebd1636f9ffaaf516cf
Felipon Lopez <pon.felipon@gmail.com>
2017-01-07 23:02:43 -0700

    agregando pruebas (no funcionan)

---
+++ pythoneyes/tests/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+
+from pythoneyes.app import create_app
+
+
+@pytest.yield_fixture(scope='session')
+def app():
+    """
+    setup our flask test app, esto se ejecuta solo una vez
+
+    :return: Flask app
+    """
+
+    params = {
+            'DEBUG': False,
+            'TESTING': True
+            }
+
+    _app = create_app(settings_override=params)
+
+    # Establish an application context before running the tests
+    ctx = _app.app_context()
+    ctx.push()
+
+    yield _app
+
+    ctx.pop()
+
+
+@pytest.yield_fixture(scope='function')
+def client():
+    '''
+    Setup an app client, this get executed for each test function
+
+    :param app: Pytest fixture
+    :return: Flask app client
+    '''
+    yield app.test_client()
